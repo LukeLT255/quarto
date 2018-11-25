e92fb607ae3af0cee0df59c72f7131552672fea6
Luuuuke <lukelt25@gmail.com>
2018-11-25 16:16:53 -0600

    quarto gamein

--- submissions/JohnAndLuke/mygames.py
+++ submissions/JohnAndLuke/mygames.py
@@ -2,15 +2,25 @@ from collections import namedtuple
 from games import (Game)
 from queue import PriorityQueue
 from copy import deepcopy
+import numpy as np
+
+
+pieces = [0b0000, 0b0001, 0b0010, 0b0011, 0b0100, 0b0101, 0b0110, 0b0111, 0b1000, 0b1001, 0b1010,
+          0b1011, 0b1100, 0b1101, 0b1110, 0b1111, ]
+
+
 
 
 class TemplateState:    # one way to define the state of a minimal game.
 
-    def __init__(self, player, board, depth=8): # add parameters as needed.
-        self.player = player
+    def __init__(self, player, board, piecesAvailable, pieceToBePlaced, pieceChosenForOpponent, label=None, depth=8): # add parameters as needed.
+        self.to_move = player
         self.board = board
         self.maxDepth = depth
-        self.label = str(id(self))   # change this to something easier to read
+        self.label = label
+        self.piecesAvailable = piecesAvailable
+        self.pieceToBePlaced = pieceToBePlaced
+        self.pieceChosenForOpponent = pieceChosenForOpponent
         # add code and self.variables as needed.
 
     def __str__(self):  # use this exact signature
@@ -36,19 +46,65 @@ class TemplateGame(Game):
     the shortest implementation I could run without errors.
     '''
     # h and v define size of board, k defines how many needed in a row to win.
-    def __init__(self, initial, h, v, k):    # add parameters if needed.
+    def __init__(self, initial, h=4, v=4, k=4):    # add parameters if needed.
         self.initial = initial
         self.h = h
         self.v = v
         self.k = k
-        self.initial = TemplateState(player='Player1', board={})
+        self.initial = TemplateState(player='Player1', board={}, piecesAvailable={}, pieceToBePlaced={}, pieceChosenForOpponent={} )
+
         # add code and self.variables if needed.
 
-    def actions(self, state):   # use this exact signature.
-        acts = []
-        # append all moves, which are legal in this state,
-        # to the list of acts.
-        return acts
+
+
+
+
+
+    def actions(self, state):
+        if state.pieceChosenForOpponent == None: #returns open spaces on the board if player has not yet placed given game piece
+            try:
+                return state.moves
+            except:
+
+                    pass
+                    "Legal moves are any square not yet taken"
+                    moves = []
+                    for x in range(1, self.h + 1):
+                        for y in range(1, self.v + 1):
+                            if (x, y) not in state.board.keys():
+                                moves.append((x, y))
+                    state.moves = moves
+                    state.pieceChosenForOpponent = "" #changing value of this var to move on to next block of code next move
+
+                    return moves
+
+        else: #returns pieces to give to opponent
+            try:
+                return state.moves
+
+            except:
+                pass
+                moves = []
+                for x in state.piecesAvailable:
+                    moves.append(x)
+                state.moves = moves
+                state.pieceChosenForOpponent = "selected"  #changes piecesChosenForOpponent to switch opponents
+                return moves
+
+
+
+    def opponent(self, player, state):
+        if player == 'Player1' and state.pieceChosenForOpponent == "selected": #If player1 has placed piece and chosen for opponent then switch players
+            state.pieceChosenForOpponent = None #resets piece chosen for opponent
+            return 'Player2'
+        if player == 'Player2' and state.pieceChosenForOpponent == "selected": #If player2 has placed piece and chosen for opponent then switch players
+            state.pieceChosenForOpponent = None #resets piece chosen for opponent
+            return 'Player1'
+        if player == 'Player1' and not state.pieceChosenForOpponent == "":   #If player 1 has placed piece, but not selected opponents piece
+            return 'Player1'
+        if player == 'Player2' and not state.pieceChosenForOpponent == "":   #If player 2 has placed piece, but not selected opponents piece
+            return 'Player2'
+        return None
 
     def result(self, state, move):   # use this exact signature.
         newState = deepcopy(state)
@@ -65,26 +121,44 @@ class TemplateGame(Game):
         <0 if the player is losing,
          0 if the state is a tie.
         '''
+
         return 0
 
-    def display(self, state):   # use this exact signature.
-        # pretty-print the game state, using ASCII art,
-        # to help a human player understand his options.
-        print(state)
+
+    def display(self, state):
+        board = state.board
+        for x in reversed(range(1, self.h + 1)):
+            for y in range(1, self.v + 1):
+                print(board.get((x, y), '....'), end='    ')
+            print('\n')
+
 
 
 myGame = TemplateGame(TemplateState, 4, 4, 4)
 
 
-# tg = TemplateGame(TemplateState(''))   # this is the game we play interactively.
+tg = TemplateGame(TemplateState('A','','','',''))   # this is the game we play interactively.
 
+
+won = TemplateState(
+    player = 'Player2',
+    board = {(1,1): '0010', (1,2): '1100', (1,3): '1010', (1,4): '1110',
+             (2,1): '1101', (2,2): '0001',
+            },
+    piecesAvailable={},
+    pieceToBePlaced={},
+    pieceChosenForOpponent=None,
+
+    label = 'won'
+)
 
 myGames = {
     myGame: [
-
+        # won,
     ],
 
-    # tg: [
-
-    # ]
+    tg: [
+        TemplateState('B','','','','')
+        # TemplateState('C'),
+    ]
 }
